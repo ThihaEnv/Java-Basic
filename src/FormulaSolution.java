fc00a786bff78ae06b69d6550d8836fd32d841f2
Suan Nyan <suanthnjobs@gmail.com>
2025-11-30 22:22:49 +0700

    (Created) Thiha committed this file.

---
+++ src/FormulaSolution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FormulaSolution {
+    public static void main(String[] args) {
+
+        // var declaration
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+
+        Scanner input = new Scanner(System.in);
+
+        //request input value
+        System.out.print("Enter radius value : ");
+        radius = input.nextDouble();
+
+        //circumference calculation
+        circumference = 2 * Math.PI * radius;
+
+        //circle area calculation
+        area = Math.PI * Math.pow(radius,2);
+
+        // circle volume calculation
+        volume = (4.0/3.0) * Math.PI * Math.pow(radius,3);
+
+        //showing answers
+        System.out.println("The circumference of the circle is " + circumference);
+        System.out.println("The area of the circle is " + area);
+        System.out.println("The volume of sphere is " + volume);
+
+        //input close
+        input.close();
+    }
+}
